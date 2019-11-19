03009d6d58d5bcec13627a3efc2be394df387c8d
iury <iury_tavares10@hotmail.com>
2019-11-19 07:42:42 -0300

    imagens adc

--- assets/javascript/introducaocomputacao.js
+++ assets/javascript/introducaocomputacao.js
@@ -4,97 +4,113 @@ $(document).ready(function () {
             question: "Na ordem cronológica, marque a alternativa correta.", 
             choice: ["Ábaco, Eniac, Chip, Transistor e Microprocessador.", "Eniac, Ábaco, Chip, Transistor e Microprocessador.", "Ábaco, Eniac, Chip, Microprocessador e Transistor.", "Ábaco, Eniac, Transistor, Chip e Microprocessador." ],
             answer: 3,
-            photoquest: "assets/images/white.png"
+            photoquest: "assets/images/white.png",
+            photo: "assets/images/Image542.jpg"            
         },
         {
             question: "A Tabela ASCII é usada pela maior parte da indústria de computadores para a troca de informações. Cada caractere é representado por um código de oito bits (um byte). Sabendo que o correspondente em ASCII do caractere A é 0100 0001 (em binário), qual o correspondente em ASCII do caractere J (em decimal)?", 
             choice: ["65", "72", "74", "80" ],
             answer: 2,
-            photoquest: "assets/images/white.png"
+            photoquest: "assets/images/white.png",
+            photo: "assets/images/Image542.jpg"
         },
         {
             question: "A conversão do número 1111 1010 1100 1110 para hexadecimal corresponde a:", 
             choice: ["FACA", "CASA", "FACE", "ABCD" ],
             answer: 2,
-            photoquest: "assets/images/white.png"
+            photoquest: "assets/images/white.png",
+            photo: "assets/images/Image542.jpg"
         },
         {
             question: "Qual o valor em decimal do binário (8 bits) 00000111, considerando a representação sem sinal?", 
             choice: ["8", "7", "13", "9" ],
             answer: 1,
-            photoquest: "assets/images/white.png"
+            photoquest: "assets/images/white.png",
+            photo: "assets/images/Image542.jpg"
         },
         {
             question: "Um administrador de sistemas, ao analisar o conteúdo de um arquivo binário, percebeu que o primeiro byte desse arquivo é, em hexadecimal, igual a 9F, que corresponde, em decimal, ao valor:", 
             choice: ["16", "159", "105", "99" ],
             answer: 1,
-            photoquest: "assets/images/white.png"
+            photoquest: "assets/images/white.png",
+            photo: "assets/images/Image542.jpg"
         },
         {
             question: "Se o sistema decimal é utilizado pelos seres humanos, o sistema binário constitui a base para a representação da informação nos computadores. Nesse contexto, um equipamento dispõe de três displays, o primeiro que mostra números em formato decimal, o segundo em binário e o terceiro em hexadecimal, havendo uma correspondência entre as representações. Se o display decimal mostra o número 250, os equivalentes em binário e em hexadecimal mostrarão, respectivamente", 
             choice: ["11111010 e FA", "11111010 e FE", "11111010 e FC", "11111110 e FE" ],
             answer: 0,
-            photoquest: "assets/images/white.png"
+            photoquest: "assets/images/white.png",
+            photo: "assets/images/Image542.jpg"
         },
         {
             question: "Em relação aos sistemas de numeração e representação de dados, analise as seguintes afirmativas: I. O número 10001 em binário corresponde ao número 17 na representação decimal. II. D na representação hexadecimal corresponde ao número 1110 em binário. III. BBB na representação hexadecimal corresponde ao número 3003 na representação decimal. Marque a alternativa CORRETA:", 
             choice: ["apenas as afirmativas I e II são verdadeiras.", "apenas as afirmativas I e III são verdadeiras.", "apenas as afirmativas II e III são verdadeiras.", "todas as afirmativas são verdadeiras." ],
             answer: 1,
-            photoquest: "assets/images/white.png"
+            photoquest: "assets/images/white.png",
+            photo: "assets/images/Image542.jpg"
         },
         {
             question: "Quais os valores em decimal do binário (8 bits) 10000001, considerando a representação sem sinal e em Complemento de 2, respectivamente?", 
             choice: ["120, -111.", "100, -100.", "129, -127.", "129, 127." ],
             answer: 2,
-            photoquest: "assets/images/white.png"
+            photoquest: "assets/images/white.png",
+            photo: "assets/images/Image542.jpg"
         },
         {
             question: "Quais os valores em decimal do binário (8 bits) 00000111, considerando a representação sem sinal e em Complemento de 2, respectivamente?", 
             choice: ["13, 7.", "13, 13.", "7, -7.", "7, 7." ],
             answer: 3,
-            photoquest: "assets/images/white.png"
+            photoquest: "assets/images/white.png",
+            photo: "assets/images/Image542.jpg"
         },
         {
             question: "Assinale a opção correta, a respeito de conceitos em processamento de dados.", 
             choice: ["Na aritmética computacional de ponto flutuante, é possível representar apenas números inteiros.", "A representação em base decimal é natural para os seres humanos. Por isto, os computadores modernos usam internamente esta base para facilitar a interação homem máquina e estimular o raciocínio integrado.", "Variáveis declaradas como ponto flutuante existem para armazenar números que possuem casas decimais, como 5,1497.", "A representação por complemento de dois é adotada apenas para representar números binários positivos." ],
             answer: 2,
-            photoquest: "assets/images/white.png"
+            photoquest: "assets/images/white.png",
+            photo: "assets/images/Image542.jpg"
         },
         {
             question: "De acordo com o padrão IEEE, um número em ponto flutuante, com precisão simples, é armazenado como S:E:F, em que S, E e F são armazenados em 1 bit, 8 bits e 23 bits, respectivamente. Qual é o valor decimal do número de ponto flutuante C1E00000 (notação hexadecimal)?", 
             choice: ["+26.", "-15.", "-26.", "-28." ],
             answer: 3,
-            photoquest: "assets/images/white.png"
+            photoquest: "assets/images/white.png",
+            photo: "assets/images/Image542.jpg"
         },
         {
             question: "Efetue a operação abaixo utilizando aritmética de complemento de 2 e marque a alternativa correspondente ao valor correto, considerando a palavra de dados com 8 bits: (+24)10 + (+15)10 .", 
             choice: ["00011001", "00100111", "11110011", "00110110" ],
             answer: 1,
-            photoquest: "assets/images/white.png"
+            photoquest: "assets/images/white.png",
+            photo: "assets/images/Image542.jpg"
         },
         {
             question: "Considere duas portas lógicas independentes, uma executa a lógica EXCLUSIVE OR (XOR), cujas entradas são A e B e a outra executa a lógica OR, cujas entradas são C e D. Se A=1, B=1 e se C=0, D=1, as saídas das portas serão, respectivamente:", 
             choice: ["0 1", "0 0", "1 0", "1 1" ],
             answer: 0,
-            photoquest: "assets/images/white.png"
+            photoquest: "assets/images/white.png",
+            photo: "assets/images/Image542.jpg"
         },
         {
             question: "Para que a saída X do circuito apresentado na figura abaixo possua valor lógico igual a 1, é necessário que as entradas A, B e C possuam os seguintes valores lógicos:", 
             choice: ["A=0; B=0; C=0.", "A=1; B=1; C=1.", "A=0; B=1; C=1.", "A=1; B=1; C=0." ],
             answer: 2,
-            photoquest: "assets/images/circ.png"
+            photoquest: "assets/images/circ.png",
+            photo: "assets/images/Image542.jpg"
         },
         {
             question: "A figura abaixo representa um circuito lógico. Este circuito lógico pode ser simplificado pela porta lógica", 
             choice: ["OR.", "XOR.", "XNOR.", "NOR." ],
             answer: 3,
-            photoquest: "assets/images/circ2.png"
+            photoquest: "assets/images/circ2.png",
+            photo: "assets/images/Image542.jpg"
         },
         {
             question: "Considere o seguinte: A=1, B=1,C=1,D=1,E=0. Qual resultado da expressão logica ((A OR C)AND B) AND E?", 
             choice: ["0", "3", "1", "2" ],
             answer: 0,
-            photoquest: "assets/images/white.png"
+            photoquest: "assets/images/white.png",
+            photo: "assets/images/Image542.jpg"
         }
 ];
     
